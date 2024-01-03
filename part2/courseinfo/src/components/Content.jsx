10959852b3f8ef768476177ffc8bb48c6ae06f0f
jakubk3697 <jakubkaminski3697@gmail.com>
2024-01-03 22:17:05 +0100

    2.2: Course information step7

--- part2/courseinfo/src/components/Content.jsx
+++ part2/courseinfo/src/components/Content.jsx
@@ -1,9 +1,17 @@
 import Part from "./Part";
 
-const Content = ({ parts }) => (
-  <div>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </div>
-)
+const Content = ({ parts }) => {
+  const totalExercises = parts.reduce((total, part) => total + part.exercises, 0);
+
+
+  return (
+    (
+      <div>
+        {parts.map(part => <Part key={part.id} part={part} />)}
+        <strong>total of {totalExercises} exercises</strong>
+      </div>
+    )
+  )
+}
 
 export default Content;
