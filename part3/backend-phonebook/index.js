0f530bbbf6f490aa281255e717c80ff561b23378
jakubk3697 <jakubkaminski3697@gmail.com>
2024-01-15 00:10:53 +0100

    3.20*: Phonebook database, step8 and 3.21 Deploying the database backend to production

--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -19,8 +19,13 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
-    minlength: 8,
-    required: true
+    validate: {
+      validator: function (v) {
+        return /^(\d{2,3})-(\d+)$/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
   }
 })
 
