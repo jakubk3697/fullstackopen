9d5fb7c3575935b6fcb59d77a9f13c41ca6564d0
jakubk3697 <jakubkaminski3697@gmail.com>
2024-01-14 21:30:48 +0100

    Exercises 3.15.-3.18.

--- part3/backend-phonebook/index.js
+++ part3/backend-phonebook/index.js
@@ -31,13 +31,23 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind == 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(express.json())
+app.use(express.static('dist'))
 app.use(morgan('tiny'));
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) });
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 app.use(cors())
-app.use(express.static('dist'))
 
 let persons = [
   {
@@ -80,19 +90,16 @@ app.get('/api/persons', (req, res) => {
   })
 })
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if (person) {
         res.json(person)
       } else {
-        res.status(400).end()
+        res.status(404).end()
       }
     })
-    .catch(error => {
-      console.log(error)
-      res.status(400).send({ error: 'malformatted id' })
-    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -100,10 +107,22 @@ app.delete('/api/persons/:id', (req, res) => {
     .then(result => {
       res.status(204).end()
     })
-    .catch(error => {
-      console.log(error)
-      res.status(400).send({ error: 'malformatted id' })
+    .catch(error => next(error))
+})
+
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then(updatedPerson => {
+      res.json(updatedPerson)
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (req, res) => {
@@ -121,6 +140,8 @@ app.post('/api/persons', (req, res) => {
     })
   }
 
+  console.log(body.name, body.number);
+
   const person = new Person({
     name: body.name,
     number: body.number
@@ -132,6 +153,7 @@ app.post('/api/persons', (req, res) => {
 })
 
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
