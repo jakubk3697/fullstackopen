78aa090f21de45f0de2c89b8bdd4739b90022da0
jakubk3697 <jakubkaminski3697@gmail.com>
2024-02-11 21:40:15 +0100

    4.5*: Helper Functions and Unit Tests, step 3

--- part4/bloglist-tests/utils/list_helper.js
+++ part4/bloglist-tests/utils/list_helper.js
@@ -2,11 +2,19 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
-const dummy = (blogs) => {
+const dummy = () => {
   return 1;
+}
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null;
+
+  const maxLikes = Math.max(...blogs.map(blog => blog.likes));
+  return blogs.find(blog => blog.likes === maxLikes);
 }
 
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
