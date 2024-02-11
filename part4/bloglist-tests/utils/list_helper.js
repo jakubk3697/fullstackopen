501a841dfbff8f31b1a4b23a5f9212145dcdbfe7
jakubk3697 <jakubkaminski3697@gmail.com>
2024-02-11 18:39:41 +0100

    4.4: helper functions and unit tests, step2

--- part4/bloglist-tests/utils/list_helper.js
+++ part4/bloglist-tests/utils/list_helper.js
@@ -1,7 +1,12 @@
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => sum + blog.likes, 0)
+}
+
 const dummy = (blogs) => {
   return 1;
 }
 
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
