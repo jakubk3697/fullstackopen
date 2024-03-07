d1810130aee28b0b58d334e103da0cd9870d3386
jakubk3697 <jakubkaminski3697@gmail.com>
2024-03-08 00:33:56 +0100

    4.10: Blog List Tests, step 3

--- part4/bloglist-tests/tests/blog_api.test.js
+++ part4/bloglist-tests/tests/blog_api.test.js
@@ -31,6 +31,27 @@ test("blog posts have 'id as identifier", async () => {
   expect(response.body[0].id).toBeDefined();
 });
 
+test("a valid blog can be added", async () => {
+  const newBlog = {
+    title: "Jest test title",
+    author: "Mr. Jest",
+    url: "www.jest.com",
+    likes: 12,
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+  const titles = blogsAtEnd.map((b) => b.title);
+  expect(titles).toContain("Jest test title");
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
