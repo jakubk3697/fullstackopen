c3ef866874c79eac22b70c592409f7b27224d3f6
jakubk3697 <jakubkaminski3697@gmail.com>
2024-03-09 21:05:13 +0100

    4.11*: Blog List Tests, step 4

--- part4/bloglist-tests/tests/blog_api.test.js
+++ part4/bloglist-tests/tests/blog_api.test.js
@@ -52,6 +52,22 @@ test("a valid blog can be added", async () => {
   expect(titles).toContain("Jest test title");
 });
 
+test("if likes property is missing, it will default to 0", async () => {
+  const newBlog = {
+    title: "Jest test title without likes",
+    author: "Mr. no likes",
+    url: "www.no-like.com",
+  };
+
+  const response = await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  expect(response.body.likes).toBe(0);
+});
+
 afterAll(async () => {
   await mongoose.connection.close();
 });
