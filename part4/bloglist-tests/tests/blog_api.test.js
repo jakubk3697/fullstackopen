41c81120dd5daf7c63a6cbb4ecf771f3b62039a9
jakubk3697 <jakubkaminski3697@gmail.com>
2024-03-09 22:09:48 +0100

    Group tests for blogs

--- part4/bloglist-tests/tests/blog_api.test.js
+++ part4/bloglist-tests/tests/blog_api.test.js
@@ -14,107 +14,115 @@ beforeEach(async () => {
   }
 });
 
-test("blogs are returned as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
+describe("when there is initially some notes saved", () => {
+  test("blogs are returned as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
+
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
+describe("when viewing a specific blog", () => {
+  test("blog posts have 'id as identifier", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body[0].id).toBeDefined();
+  });
 });
 
-test("blog posts have 'id as identifier", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body[0].id).toBeDefined();
+describe("when addition of a new note", () => {
+  test("a valid blog can be added", async () => {
+    const newBlog = {
+      title: "Jest test title",
+      author: "Mr. Jest",
+      url: "www.jest.com",
+      likes: 12,
+    };
+
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+
+    const titles = blogsAtEnd.map((b) => b.title);
+    expect(titles).toContain("Jest test title");
+  });
+
+  test("if likes property is missing, it will default to 0", async () => {
+    const newBlog = {
+      title: "Jest test title without likes",
+      author: "Mr. no likes",
+      url: "www.no-like.com",
+    };
+
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body.likes).toBe(0);
+  });
+
+  test("if title and url properties are missing, it will return 400", async () => {
+    const newBlog = {
+      title: "Jest test title without likes",
+      author: "Mr. no likes",
+      url: "www.no-like.com",
+      likes: 19,
+    };
+
+    const response = await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+
+    expect(response.body.title).toBeDefined();
+    expect(response.body.url).toBeDefined();
+  });
 });
 
-test("a valid blog can be added", async () => {
-  const newBlog = {
-    title: "Jest test title",
-    author: "Mr. Jest",
-    url: "www.jest.com",
-    likes: 12,
-  };
+describe("when deleting or updating blogs", () => {
+  test("a blog can be deleted", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToDelete = blogsAtStart[0];
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
 
-  const titles = blogsAtEnd.map((b) => b.title);
-  expect(titles).toContain("Jest test title");
-});
+    const titles = blogsAtEnd.map((r) => r.title);
+    expect(titles).not.toContain(blogToDelete.title);
+  });
 
-test("if likes property is missing, it will default to 0", async () => {
-  const newBlog = {
-    title: "Jest test title without likes",
-    author: "Mr. no likes",
-    url: "www.no-like.com",
-  };
+  test("a blog can be updated", async () => {
+    const blogsAtStart = await helper.blogsInDb();
+    const blogToUpdate = blogsAtStart[0];
 
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    const updatedBlog = { ...blogToUpdate, likes: blogToUpdate.likes + 1 };
 
-  expect(response.body.likes).toBe(0);
-});
+    await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedBlog)
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
 
-test("if title and url properties are missing, it will return 400", async () => {
-  const newBlog = {
-    title: "Jest test title without likes",
-    author: "Mr. no likes",
-    url: "www.no-like.com",
-    likes: 19,
-  };
+    const blogsAtEnd = await helper.blogsInDb();
+    expect(blogsAtEnd[0].likes).toBe(blogToUpdate.likes + 1);
+  });
 
-  const response = await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  expect(response.body.title).toBeDefined();
-  expect(response.body.url).toBeDefined();
-});
-
-test("a blog can be deleted", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToDelete = blogsAtStart[0];
-
-  await api.delete(`/api/blogs/${blogToDelete.id}`).expect(204);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length - 1);
-
-  const titles = blogsAtEnd.map((r) => r.title);
-  expect(titles).not.toContain(blogToDelete.title);
-});
-
-test("a blog can be updated", async () => {
-  const blogsAtStart = await helper.blogsInDb();
-  const blogToUpdate = blogsAtStart[0];
-
-  const updatedBlog = { ...blogToUpdate, likes: blogToUpdate.likes + 1 };
-
-  await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send(updatedBlog)
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd[0].likes).toBe(blogToUpdate.likes + 1);
-});
-
-afterAll(async () => {
-  await mongoose.connection.close();
+  afterAll(async () => {
+    await mongoose.connection.close();
+  });
 });
