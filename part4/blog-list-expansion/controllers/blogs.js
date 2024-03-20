bbea8e4bc28ad00ff4fac9dbaced09b97c275d06
jakubk3697 <jakubkaminski3697@gmail.com>
2024-03-20 22:42:17 +0100

    Exercises 4.20-4.21

--- part4/blog-list-expansion/controllers/blogs.js
+++ part4/blog-list-expansion/controllers/blogs.js
@@ -14,11 +14,16 @@ blogsRouter.get("/", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
+  const user = request.user
+
+  if (!user) {
     return response.status(401).json({ error: "token invalid" });
   }
-  const user = await User.findById(decodedToken.id);
+
+  const existingBlog = await Blog.findOne({ title: body.title });
+  if (existingBlog) {
+    return response.status(400).json({ error: "blog already exists" });
+  }
 
   const blog = new Blog({
     ...body,
@@ -41,11 +46,12 @@ blogsRouter.post("/", async (request, response) => {
 });
 
 blogsRouter.delete("/:id", async (request, response) => {
-  const decodedToken = jwt.verify(request.token, process.env.SECRET);
-  if (!decodedToken.id) {
+  const user = request.user
+
+  if (!user) {
     return response.status(401).json({ error: "token invalid" });
   }
-  const user = await User.findById(decodedToken.id);
+
   const blog = await Blog.findById(request.params.id);
 
   if (blog.user.toString() === user.id.toString()) {
