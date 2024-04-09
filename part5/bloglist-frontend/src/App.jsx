ada7cf395b7b72024aedb0272c6f0139db2dedd3
jakubk3697 <jakubkaminski3697@gmail.com>
2024-04-10 00:23:40 +0200

    5.3: Blog List Frontend, step 3 & 5.4: Blog List Frontend, step 4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -1,48 +1,95 @@
-import { useState, useEffect } from 'react'
-import Blog from './components/Blog'
-import blogService from './services/blogs'
-import loginService from './services/login'
+import { useState, useEffect } from "react";
+import Blog from "./components/Blog";
+import blogService from "./services/blogs";
+import loginService from "./services/login";
 
 const App = () => {
-  const [blogs, setBlogs] = useState([])
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [user, setUser] = useState(null)
+  const [blogs, setBlogs] = useState([]);
+  const [newBlog, setNewBlog] = useState({
+    title: "",
+    author: "",
+    url: "",
+  });
+  const [notification, setNotification] = useState({
+    message: null,
+    color: "green",
+  });
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    blogService.getAll().then(blogs =>
-      setBlogs(blogs)
-    )
-  }, [])
+    blogService.getAll().then((blogs) => setBlogs(blogs));
+  }, []);
 
   useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
     if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      // blogService.setToken(user.token)
+      const user = JSON.parse(loggedUserJSON);
+      setUser(user);
+      blogService.setToken(user.token);
     }
-  }, [])
+  }, []);
+
+  useEffect(() => {
+    if (notification.message) {
+      const timer = setTimeout(() => {
+        setNotification({ message: null, color: "green" });
+      }, 5000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [notification]);
 
   const handleLogin = async (event) => {
-    event.preventDefault()
+    event.preventDefault();
     try {
       const user = await loginService.login({
-        username, password,
-      })
-      window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
-      setUser(user)
-      setUsername('')
-      setPassword('')
+        username,
+        password,
+      });
+      window.localStorage.setItem("loggedBlogappUser", JSON.stringify(user));
+      blogService.setToken(user.token);
+      setUser(user);
+      setUsername("");
+      setPassword("");
     } catch (exception) {
-      console.log('wrong credentials')
+      setNotification({ message: "wrong username or password", color: "red" });
+      console.log("wrong credentials");
     }
-  }
+  };
+  const addBlog = async (event) => {
+    event.preventDefault();
+    const blogObject = {
+      title: newBlog.title,
+      author: newBlog.author,
+      url: newBlog.url,
+    };
+
+    try {
+      const returnedBlog = await blogService.create(blogObject);
+      setBlogs(blogs.concat(returnedBlog));
+      setNewBlog({
+        title: "",
+        author: "",
+        url: "",
+      });
+      setNotification({
+        message: `a new blog ${returnedBlog.title} by ${returnedBlog.author} added`,
+        color: "green",
+      });
+    } catch (exception) {
+      setNotification({ message: "error adding blog", color: "red" });
+    }
+  };
 
   const loginForm = () => {
     return (
       <>
         <h2>log in to application</h2>
+        {notification.message && (
+          <p style={{ color: notification.color }}>{notification.message}</p>
+        )}
         <form onSubmit={handleLogin}>
           <div>
             username
@@ -65,33 +112,70 @@ const App = () => {
           <button type="submit">login</button>
         </form>
       </>
-    )
-  }
+    );
+  };
 
   const blogForm = () => {
     return (
       <div>
         <h2>blogs</h2>
-        <p>{user.name} logged in</p>
-        <button onClick={() => {
-          window.localStorage.removeItem('loggedBlogappUser');
-          setUser(null);
-        }}>logout</button>
-        {blogs.map(blog =>
-          <Blog key={blog.id} blog={blog} />
+        {notification.message && (
+          <p style={{ color: notification.color }}>{notification.message}</p>
         )}
+        <p>{user.name} logged in</p>
+        <button
+          onClick={() => {
+            window.localStorage.removeItem("loggedBlogappUser");
+            setUser(null);
+          }}
+        >
+          logout
+        </button>
+
+        <h2>create new</h2>
+        <form onSubmit={addBlog}>
+          <div>
+            title:
+            <input
+              type="text"
+              value={newBlog.title}
+              name="Title"
+              onChange={({ target }) =>
+                setNewBlog({ ...newBlog, title: target.value })
+              }
+            />{" "}
+            <br />
+            author:
+            <input
+              type="text"
+              value={newBlog.author}
+              name="Author"
+              onChange={({ target }) =>
+                setNewBlog({ ...newBlog, author: target.value })
+              }
+            />{" "}
+            <br />
+            url:
+            <input
+              type="text"
+              value={newBlog.url}
+              name="Url"
+              onChange={({ target }) =>
+                setNewBlog({ ...newBlog, url: target.value })
+              }
+            />
+          </div>
+          <button type="submit">create</button>
+        </form>
+
+        {blogs.map((blog) => (
+          <Blog key={blog.id} blog={blog} />
+        ))}
       </div>
-    )
-  }
+    );
+  };
 
-  return (
-    <>
-      {user === null ?
-        loginForm() :
-        blogForm()
-      }
-    </>
-  )
-}
+  return <>{user === null ? loginForm() : blogForm()}</>;
+};
 
-export default App
+export default App;
--- part5/bloglist-frontend/src/services/blogs.js
+++ part5/bloglist-frontend/src/services/blogs.js
@@ -1,9 +1,36 @@
-import axios from 'axios'
-const baseUrl = '/api/blogs'
+import axios from "axios";
+const baseUrl = "/api/blogs";
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
-}
+let token = null;
 
-export default { getAll }
+const setToken = (newToken) => {
+  token = `Bearer ${newToken}`;
+};
+
+const getAll = async () => {
+  const request = axios.get(baseUrl);
+  const response = await request;
+  return response.data;
+};
+
+const create = async (newObject) => {
+  const config = {
+    headers: { Authorization: token },
+  };
+
+  const response = await axios.post(baseUrl, newObject, config);
+  return response.data;
+};
+
+const update = async (id, newObject) => {
+  const request = axios.put(`${baseUrl}/${id}`, newObject);
+  const response = await request;
+  return response.data;
+};
+
+export default {
+  getAll,
+  create,
+  update,
+  setToken,
+};
