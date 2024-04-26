58377304882fdc7ce451946bd493b8516e13863d
jakubk3697 <jakubkaminski3697@gmail.com>
2024-04-26 21:49:33 +0200

    6.5: Anecdotes, step 3

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -4,6 +4,8 @@ const App = () => {
   const anecdotes = useSelector((state) => state);
   const dispatch = useDispatch();
 
+  const sortedAnecdotes = anecdotes.sort((a, b) => b.votes - a.votes);
+
   const vote = (id) => {
     dispatch({ type: "VOTE", data: { id } });
   };
@@ -18,7 +20,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map((anecdote) => (
+      {sortedAnecdotes.map((anecdote) => (
         <div key={anecdote.id}>
           <div>{anecdote.content}</div>
           <div>
