9b5f244150e0a2f3f6d606f0b44361729fbb8c94
Jakub <jakubkaminski3697@gmail.com>
2025-02-19 22:08:30 +0100

    Production compose

--- part12/3/todo-frontend/.eslintrc.cjs
+++ part12/3/todo-frontend/.eslintrc.cjs
@@ -13,6 +13,8 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
+    'react/prop-types': 'off',
+    'react/jsx-key': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
